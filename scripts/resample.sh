c559936973fa5b1b71d536dc99218f1828aa5418
Ties Jan Hefting <hello@tiesjan.com>
2025-11-26 22:30:00 +0100

    Update resample script
    - Add option `--force` to enforce resampling even if the file already
      has the correct sampling format and rate. This is useful when
      re-compressing FLAC files without quality loss.
    - Take over modification date from original file when writing the output
      file. This can be disabled using the added option `no-preserve-mtime`.
    - Revised formatting for increased readability.

--- scripts/resample.sh
+++ scripts/resample.sh
@@ -3,9 +3,13 @@
 ORIGINAL_FILENAME_PREFIX="ORIGINAL_"
 RESAMPLING_FILENAME_PREFIX="RESAMPLING_"
 
+OUTPUT_SAMPLE_FORMAT="s16"
+OUTPUT_SAMPLE_RATE=44100
+
 
 bail () {
-    if [ $# -gt 0 ]; then
+    if [ $# -gt 0 ]
+    then
         printf "\nError! %s\n" "$@" >&2
     fi
 
@@ -19,15 +23,20 @@ warn () {
 
 
 resample_file () {
-    if [ $# -ne 3 ]; then
-        bail "Function \`resample_file()\` expected 3 arguments, $# were given."
+    if [ $# -ne 5 ]
+    then
+        bail "Function \`resample_file()\` expected 5 arguments, $# were given."
     fi
 
     file_path="$1"
-    working_dir="$(dirname "${file_path}")"
+    audio_format="$2"
+    audio_format_args="$3"
+    force_resampling="$4"
+    preserve_mtime="$5"
 
     # Retrieve audio stream information from file
-    if ! file_info=$(ffprobe -v quiet -print_format json -show_streams "${file_path}"); then
+    if ! file_info=$(ffprobe -v quiet -print_format json -show_streams "${file_path}")
+    then
         warn "Not a media file: ${file_path}"
         return
     fi
@@ -36,32 +45,22 @@ resample_file () {
     sample_format=$(printf "%s" "${stream_info}" | jq --raw-output '.sample_fmt')
     sample_rate=$(( $(printf "%s" "${stream_info}" | jq --raw-output '.sample_rate | tonumber') ))
 
-    needs_resampling=false
-
-    # Determine sampling format (bit depth)
-    if [ "${sample_format}" != "s16" ]; then
-        sample_format="s16"
-
-        needs_resampling=true
-    fi
-
-    # Determine sample rate
-    if [ ${sample_rate} -ne 44100 ]; then
-        sample_rate=44100
-
-        needs_resampling=true
-    fi
-
-    # Quit early if resampling is not needed
-    if [ "${needs_resampling}" = false ]; then
-        return
+    if [ "${force_resampling}" = false ]
+    then
+        # Quit early if the output sample format and sample rate match the input file
+        if [ "${sample_format}" = "${OUTPUT_SAMPLE_FORMAT}" ] || [ "${sample_rate}" -eq ${OUTPUT_SAMPLE_RATE} ]
+        then
+            printf "Skipping %s\n" "${file_path}"
+            return
+        fi
     fi
 
     printf "Resampling %s...\n" "${file_path}"
 
     # Resample file
-    audio_format="$2"
-    audio_format_args="$3"
+    sample_format="${OUTPUT_SAMPLE_FORMAT}"
+    sample_rate="${OUTPUT_SAMPLE_RATE}"
+    working_dir="$(dirname "${file_path}")"
     output_file_path="${working_dir}/${RESAMPLING_FILENAME_PREFIX}$(basename "${file_path}")"
     ffmpeg \
         -v quiet \
@@ -73,27 +72,40 @@ resample_file () {
         -codec:v copy \
         "${output_file_path}"
 
+    original_file_path="${working_dir}/${ORIGINAL_FILENAME_PREFIX}$(basename "${file_path}")"
+
     # Keep original audio file with prefix in the filename
-    mv "${file_path}" "${working_dir}/${ORIGINAL_FILENAME_PREFIX}$(basename "${file_path}")"
+    mv "${file_path}" "${original_file_path}"
 
     # Replace original file with resampled audio file
     mv "${output_file_path}" "${file_path}"
+
+    if [ "${preserve_mtime}" = true ]
+    then
+        # Take over the modification time from the original file
+        touch -r "${original_file_path}" "${file_path}"
+    fi
 }
 
 
 resample_dir () {
-    if [ $# -ne 4 ]; then
-        bail "Function \`resample_file()\` expected 4 arguments, $# were given."
+    if [ $# -ne 6 ]
+    then
+        bail "Function \`resample_file()\` expected 6 arguments, $# were given."
     fi
 
     dir_path="$1"
     file_extension="$2"
     audio_format="$3"
     audio_format_args="$4"
+    force_resampling="$5"
+    preserve_mtime="$6"
 
-    for file_path in "${dir_path}"/*."${file_extension}"; do
+    for file_path in "${dir_path}"/*."${file_extension}"
+    do
         # Skip non-regular files and symlinks
-        if [ ! -f "${file_path}" ] || [ -L "${file_path}" ]; then
+        if [ ! -f "${file_path}" ] || [ -L "${file_path}" ]
+        then
             continue
         fi
 
@@ -107,26 +119,73 @@ resample_dir () {
                 ;;
         esac
 
-        resample_file "${file_path}" "${audio_format}" "${audio_format_args}"
+        resample_file \
+            "${file_path}" \
+            "${audio_format}" \
+            "${audio_format_args}" \
+            "${force_resampling}" \
+            "${preserve_mtime}"
     done
 }
 
 
 print_usage () {
-    printf "Usage: resample <FORMAT> <PATH>\n"
+    printf "Usage: resample [--force | --no-preserve-mtime] <FORMAT> <PATH>\n"
     printf "  <FORMAT> is one of the supported output formats: aiff, flac\n"
     printf "  <PATH> is the path to a directory or a regular file\n"
+    printf "  --force will resample regardless of the target sampling format or rate\n"
+    printf "  --no-preserve-mtime will not take over the modification time from the original file\n"
 }
 
 
 main () {
-    if [ $# -ne 2 ]; then
+    output_format=""
+    path=""
+
+    force_resampling=false
+    preserve_mtime=true
+
+    positional_argument_count=0
+    while [ $# -gt 0 ]
+    do
+        case "$1" in
+            "--force")
+                force_resampling=true
+                shift
+                ;;
+            "--no-preserve-mtime")
+                preserve_mtime=false
+                shift
+                ;;
+            "--"*)
+                print_usage
+                bail "Unknown argument: '$1'."
+                ;;
+            *)
+                if [ ${positional_argument_count} -eq 0 ]
+                then
+                    output_format="$1"
+                    positional_argument_count=$((positional_argument_count+1))
+                    shift
+                elif [ ${positional_argument_count} -eq 1 ]
+                then
+                    path="$1"
+                    positional_argument_count=$((positional_argument_count+1))
+                    shift
+                else
+                    print_usage
+                    bail "Too many positional arguments."
+
+                fi
+                ;;
+        esac
+    done
+
+    if [ "${positional_argument_count}" -ne 2 ]
+    then
         print_usage
         bail
     fi
-
-    output_format="$1"
-    path="$2"
 
     # Validate audio format
     case "${output_format}" in
@@ -135,13 +194,11 @@ main () {
             audio_format_args="-write_id3v2 1"
             file_extension="${output_format}"
             ;;
-
         "flac")
             audio_format="${output_format}"
             audio_format_args="-compression_level 12"
             file_extension="${output_format}"
             ;;
-
         *)
             print_usage
             bail "Given <FORMAT> is not a valid format: '${output_format}'"
@@ -149,12 +206,25 @@ main () {
     esac
 
     # Handle regular, non-symlink files
-    if [ -f "${path}" ] && [ ! -L "${path}" ]; then
-        resample_file "${path}" "${audio_format}" "${audio_format_args}"
+    if [ -f "${path}" ] && [ ! -L "${path}" ]
+    then
+        resample_file \
+            "${path}" \
+            "${audio_format}" \
+            "${audio_format_args}" \
+            "${force_resampling}" \
+            "${preserve_mtime}"
 
     # Handle directories
-    elif [ -d "${path}" ]; then
-        resample_dir "${path}" "${file_extension}" "${audio_format}" "${audio_format_args}"
+    elif [ -d "${path}" ]
+    then
+        resample_dir \
+            "${path}" \
+            "${file_extension}" \
+            "${audio_format}" \
+            "${audio_format_args}" \
+            "${force_resampling}" \
+            "${preserve_mtime}"
 
     else
         print_usage
