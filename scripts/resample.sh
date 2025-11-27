08544633f01b5b09db3f2f332718897ce5adc318
Ties Jan Hefting <hello@tiesjan.com>
2025-11-27 22:00:00 +0100

    More updates to audio resampling script
    - Drop support for iterating over directories for simpler logic.
    - Stricten input audio format to be the same as the output audio format,
      to allow resampling files of the same media file type only.

--- scripts/resample.sh
+++ scripts/resample.sh
@@ -18,26 +18,33 @@ bail () {
 
 
 warn () {
-    printf "Warning! %s\n" "$@" >&2
+    printf "%s\n" "$@" >&2
 }
 
 
 resample_file () {
-    if [ $# -ne 5 ]
+    if [ $# -ne 6 ]
     then
-        bail "Function \`resample_file()\` expected 5 arguments, $# were given."
+        bail "Function \`resample_file()\` expected 6 arguments, $# were given."
     fi
 
     file_path="$1"
-    audio_format="$2"
-    audio_format_args="$3"
-    force_resampling="$4"
-    preserve_mtime="$5"
+    file_extension="$2"
+    audio_format="$3"
+    audio_format_args="$4"
+    force_resampling="$5"
+    preserve_mtime="$6"
 
     # Retrieve audio stream information from file
     if ! file_info=$(ffprobe -v quiet -print_format json -show_streams "${file_path}")
     then
-        warn "Not a media file: ${file_path}"
+        warn "Not a media file: '${file_path}'"
+        return
+    fi
+
+    if [ "${file_path}" != "${file_path%.*}.${file_extension}" ]
+    then
+        warn "File has a different audio format than '${audio_format}': '${file_path}'"
         return
     fi
 
@@ -88,64 +95,24 @@ resample_file () {
 }
 
 
-resample_dir () {
-    if [ $# -ne 6 ]
-    then
-        bail "Function \`resample_file()\` expected 6 arguments, $# were given."
-    fi
-
-    dir_path="$1"
-    file_extension="$2"
-    audio_format="$3"
-    audio_format_args="$4"
-    force_resampling="$5"
-    preserve_mtime="$6"
-
-    for file_path in "${dir_path}"/*."${file_extension}"
-    do
-        # Skip non-regular files and symlinks
-        if [ ! -f "${file_path}" ] || [ -L "${file_path}" ]
-        then
-            continue
-        fi
-
-        # Skip files that have the filename prefixes
-        case "$(basename "${file_path}")" in
-            ${ORIGINAL_FILENAME_PREFIX}*)
-                continue
-                ;;
-            ${RESAMPLING_FILENAME_PREFIX}*)
-                continue
-                ;;
-        esac
-
-        resample_file \
-            "${file_path}" \
-            "${audio_format}" \
-            "${audio_format_args}" \
-            "${force_resampling}" \
-            "${preserve_mtime}"
-    done
-}
-
-
 print_usage () {
-    printf "Usage: resample [--force | --no-preserve-mtime] <FORMAT> <PATH>\n"
-    printf "  <FORMAT> is one of the supported output formats: aiff, flac\n"
-    printf "  <PATH> is the path to a directory or a regular file\n"
+    printf "Resample files if they have sampling formats other than 16-bit signed integer and/or \n"
+    printf "sampling rates other than 44100Hz.\n"
+    printf "\n"
+    printf "Usage: resample [--force | --no-preserve-mtime] <FORMAT> <FILE> [<FILE>...]\n"
     printf "  --force will resample regardless of the target sampling format or rate\n"
     printf "  --no-preserve-mtime will not take over the modification time from the original file\n"
+    printf "  <FORMAT> is one of the supported output formats: aiff, flac\n"
+    printf "  <FILE> is the path to a file to resample\n"
 }
 
 
 main () {
     output_format=""
-    path=""
 
     force_resampling=false
     preserve_mtime=true
 
-    positional_argument_count=0
     while [ $# -gt 0 ]
     do
         case "$1" in
@@ -162,26 +129,15 @@ main () {
                 bail "Unknown argument: '$1'."
                 ;;
             *)
-                if [ ${positional_argument_count} -eq 0 ]
-                then
-                    output_format="$1"
-                    positional_argument_count=$((positional_argument_count+1))
-                    shift
-                elif [ ${positional_argument_count} -eq 1 ]
-                then
-                    path="$1"
-                    positional_argument_count=$((positional_argument_count+1))
-                    shift
-                else
-                    print_usage
-                    bail "Too many positional arguments."
-
-                fi
+                output_format="$1"
+                shift
+                break
                 ;;
         esac
     done
 
-    if [ "${positional_argument_count}" -ne 2 ]
+    # No paths have been given when there are no more arguments left
+    if [ "$#" -eq 0 ]
     then
         print_usage
         bail
@@ -205,31 +161,21 @@ main () {
             ;;
     esac
 
-    # Handle regular, non-symlink files
-    if [ -f "${path}" ] && [ ! -L "${path}" ]
-    then
-        resample_file \
-            "${path}" \
-            "${audio_format}" \
-            "${audio_format_args}" \
-            "${force_resampling}" \
-            "${preserve_mtime}"
-
-    # Handle directories
-    elif [ -d "${path}" ]
-    then
-        resample_dir \
-            "${path}" \
-            "${file_extension}" \
-            "${audio_format}" \
-            "${audio_format_args}" \
-            "${force_resampling}" \
-            "${preserve_mtime}"
-
-    else
-        print_usage
-        bail "Given <PATH> is not a directory or a regular file: '${path}'"
-    fi
+    for file_path in "$@"
+    do
+        if [ -f "${file_path}" ] && [ ! -L "${file_path}" ]
+        then
+            resample_file \
+                "${file_path}" \
+                "${file_extension}" \
+                "${audio_format}" \
+                "${audio_format_args}" \
+                "${force_resampling}" \
+                "${preserve_mtime}"
+        else
+            warn "Path is not a (regular) file: '${file_path}'"
+        fi
+    done
 }
 
 
